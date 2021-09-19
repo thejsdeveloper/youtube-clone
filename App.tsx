4199f173222dd910ee797b01cfc0736cb4d9c612
vikas yadav <thejsdeveloper@gmail.com>
2021-09-19 12:07:31 +0530

    adding styled component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "styled-components/native";
 
 import { Navigation } from "./src/infrastructure/Navigation";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Navigation />
+      <ThemeProvider theme={theme}>
+        <Navigation />
+      </ThemeProvider>
       <StatusBar />
     </SafeAreaProvider>
   );
