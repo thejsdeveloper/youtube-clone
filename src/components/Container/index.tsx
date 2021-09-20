4d93e18219f2beeae523c4aecbef2166f30a2a95
vikas yadav <thejsdeveloper@gmail.com>
2021-09-20 22:32:52 +0530

    changing file name

--- src/components/Container/index.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from "../../infrastructure/theme/styled-component";
-
-export const Container = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.colors.ui.primary};
-`;
