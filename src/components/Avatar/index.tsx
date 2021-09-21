ecd4e92e6aeb916f79bc10370a2dad1461dc1af6
vikas yadav <thejsdeveloper@gmail.com>
2021-09-22 00:08:51 +0530

    adding units to border radius

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,5 +3,5 @@ import { styled } from "../../infrastructure/theme/styled-component";
 export const Avatar = styled.Image`
   width: 50px;
   height: 50px;
-  border-radius: 999;
+  border-radius: 9999px;
 `;
