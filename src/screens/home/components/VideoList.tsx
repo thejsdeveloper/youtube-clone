5211ee534c617779971cbe99e130bf14917df901
vikas yadav <thejsdeveloper@gmail.com>
2021-09-22 00:27:53 +0530

    completing viedo list

--- src/screens/home/components/VideoList.tsx
+++ src/screens/home/components/VideoList.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { Video } from "../../../../models";
-import data from "../../../../assets/data/video.json";
+import data from "../../../../assets/data/videos.json";
 import { VideoListItem } from "../../../components/VideoListItem";
-import {
-  TimeContainer,
-  TimeText,
-} from "../../../components/VideoListItem/styles";
-import { Avatar } from "../../../components//Avatar";
 
 export const VideoList = () => {
-  const video = data as unknown as Video;
+  const videos = data as unknown as Video[];
 
   return (
     <View>
-      <VideoListItem video={video} />
+      <FlatList
+        data={videos}
+        renderItem={({ item }) => <VideoListItem video={item} />}
+      />
     </View>
   );
 };
